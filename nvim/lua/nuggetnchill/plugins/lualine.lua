56f7e51b3eb0c2b759ae368d4c5e6c71d089717e
Nugget <nuggetnchill@gmail.com>
2022-12-04 17:53:34 -0600

    noice

--- nvim/lua/nuggetnchill/plugins/lualine.lua
+++ nvim/lua/nuggetnchill/plugins/lualine.lua
@@ -1,7 +1,7 @@
 -- import lualine plugin safely
 local status, lualine = pcall(require, "lualine")
 if not status then
-  return
+	return
 end
 
 -- get lualine nightfly theme
@@ -37,59 +37,61 @@ end
 
 -- lualine Bubble config for lualine
 local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#79dac8',
-  black  = '#080808',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#7f43cc',
-  grey   = '#303030',
+	blue = "#80a0ff",
+	cyan = "#79dac8",
+	black = "#1a1919",
+	white = "#c6c6c6",
+	red = "#ff5189",
+	violet = "#7f43cc",
+	grey = "#303030",
+	orange = "#e89a25",
 }
 
 local bubbles_theme = {
-  normal = {
-    a = { fg = colors.black, bg = colors.violet },
-    b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.black, bg = colors.black },
-  },
+	normal = {
+		a = { fg = colors.black, bg = colors.orange },
+		b = { fg = colors.cyan, bg = colors.grey },
+		c = { fg = colors.grey, bg = colors.black },
+	},
 
-  insert = { a = { fg = colors.black, bg = colors.blue } },
-  visual = { a = { fg = colors.black, bg = colors.cyan } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
+	insert = { a = { fg = colors.black, bg = colors.blue } },
+	command = { a = { fg = colors.black, bg = colors.violet } },
+	visual = { a = { fg = colors.black, bg = colors.cyan } },
+	replace = { a = { fg = colors.black, bg = colors.red } },
 
-  inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.black, bg = colors.black },
-  },
+	inactive = {
+		a = { fg = colors.white, bg = colors.grey },
+		b = { fg = colors.white, bg = colors.grey },
+		c = { fg = colors.white, bg = colors.grey },
+	},
 }
 
-require('lualine').setup {
-  options = {
-    theme = bubbles_theme,
-    component_separators = '|',
-    section_separators = { left = '', right = '' },
-  },
-  sections = {
-    lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 2 },
-    },
-    lualine_b = { 'filename', 'branch' },
-    lualine_c = { 'fileformat' },
-    lualine_x = {},
-    lualine_y = { 'filetype', 'progress' },
-    lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
-    },
-  },
-  inactive_sections = {
-    lualine_a = { 'filename' },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { 'location' },
-  },
-  tabline = {},
-  extensions = {},
-}
+require("lualine").setup({
+	options = {
+		theme = bubbles_theme,
+		component_separators = "|",
+		section_separators = { left = "", right = "" },
+	},
+	sections = {
+		lualine_a = {
+			{ "mode", separator = { left = "" }, right_padding = 2 },
+		},
+		lualine_b = { "filename", "branch" },
+		lualine_c = { "fileformat" },
+		lualine_x = { "diff" },
+		lualine_y = { "filetype", "progress" },
+		lualine_z = {
+			{ "location", separator = { right = "" }, left_padding = 2 },
+		},
+	},
+	inactive_sections = {
+		lualine_a = { "filename" },
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = { "filetype" },
+	},
+	tabline = {},
+	extensions = {},
+})
